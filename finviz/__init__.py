404040c119a1bfa427e72b389604ac6ce4382cd1
ydnrypls <slpyrndy@gmail.com>
2021-07-14 23:04:14 -0400

    Update __init__.py

--- finviz/__init__.py
+++ finviz/__init__.py
@@ -1,4 +1,4 @@
 from finviz.main_func import (get_all_news, get_analyst_price_targets,
-                              get_insider, get_news, get_stock)
+                              get_insider, get_news, get_stock, get_profile)
 from finviz.portfolio import Portfolio
 from finviz.screener import Screener
